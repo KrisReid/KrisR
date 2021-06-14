6a95d80171a38541bf4769a5d07101310e9300ab
Kris Reid <krisreid_06@hotmail.com>
2021-06-14 18:44:04 +0100

    Initial Commit

---
+++ Shared/KrisRApp.swift
@@ -0,0 +1,17 @@
+//
+//  KrisRApp.swift
+//  Shared
+//
+//  Created by Kris Reid on 14/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct KrisRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
